36d89abd5a84a7bbce62a33168f2e891b3123e7f
Theodore Kisner <tskisner.public@gmail.com>
2020-06-06 23:25:00 -0700

    More debugging on linux

--- test_scripts/install_deps_cibuildwheel_linux.sh
+++ test_scripts/install_deps_cibuildwheel_linux.sh
@@ -11,7 +11,3 @@ popd >/dev/null 2>&1
 # Install mpich
 yum -y update
 yum -y install mpich-3.2-devel.x86_64 mpich-3.2-autoload.x86_64
-
-# Load MPI compiler
-source /etc/profile.d/modules.sh
-source /etc/profile.d/mpich-3.2-x86_64.sh
